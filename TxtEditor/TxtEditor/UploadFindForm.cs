84093cd6af733d56553b3f9568cb745ae9da8af7
Anastasiia Lopata <lopata.ad@gmail.com>
2023-05-09 18:43:53 +0200

    created find word method using string basic methods

--- TxtEditor/TxtEditor/UploadFindForm.cs
+++ TxtEditor/TxtEditor/UploadFindForm.cs
@@ -4,12 +4,15 @@ using System.Windows.Forms;
 namespace TxtEditor
 {
     sealed partial class UploadFindForm : Form
-    {
+    {// public static UploadFindForm findForm;
         private static UploadFindForm _instance;
         private static readonly object _lock = new object();
+      //  public static HashTextFromTextBox hashText;
+      public string FindWords { get; private set; }
         private UploadFindForm()
         {
             InitializeComponent();
+          //  hashText = new HashTextFromTextBox(10);
         }
 
         public static UploadFindForm GetInstance()
@@ -27,18 +30,40 @@ namespace TxtEditor
             return _instance;
         }
 
-        
-        
+
+
         private void UploadFindForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             //cancel the closing event from the closing the for
-            e.Cancel= true;
+            e.Cancel = true;
             this.Hide();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+
+      /*  private void HashTextBox()
+        {
+            var splitRow = hashText.Split(MainForm.textFromTexBox);
+            foreach (var item in splitRow)
+            {
+                hashText.Add(item);
+
+            }
+        }*/
+
+        private void button_find_Click(object sender, EventArgs e)
+        {
+            var str = textBox_find.Text;            
+            string str2 = MainForm.textFromTexBox;
+            if (str2.Contains(str))
+            {
+                FindWords = str2.Replace( str,str.ToUpper());
+            }
+
         }
     }
 }
